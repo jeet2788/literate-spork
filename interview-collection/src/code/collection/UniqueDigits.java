4940578d640beef029b986649329150465c1beaa
jeet2788 <opensourcebyjeet@gmail.com>
2026-04-13 00:49:19 +0530

    added

---
+++ interview-collection/src/code/collection/UniqueDigits.java
@@ -0,0 +1,43 @@
+package code.collection;
+
+import java.util.*;
+
+public class UniqueDigits {
+
+    public static void main(String[] args) {
+        UniqueDigits obj = new UniqueDigits();
+
+        int low = 10;
+        int high = 50;
+
+        List<Integer> result = obj.uniqueDigitNumbers(low, high);
+
+        System.out.println("Numbers with unique digits between " + low + " and " + high + ":");
+        System.out.println(result);
+    }
+
+    public List<Integer> uniqueDigitNumbers(int low, int high) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num = low; num <= high; num++) {
+            if (hasUniqueDigits(num)) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
+
+    private boolean hasUniqueDigits(int num) {
+        boolean[] seen = new boolean[10];
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (seen[digit]) return false;
+
+            seen[digit] = true;
+            num /= 10;
+        }
+        return true;
+    }
+}
